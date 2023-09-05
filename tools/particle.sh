a9c0567943102920f25443deabd0eace5211a4cf
Maria Kleiner <mashkaaaa@gmail.com>
2023-09-04 22:42:55 -0700

    update licences

--- tools/particle.sh
+++ tools/particle.sh
@@ -2,9 +2,8 @@
 cat > $1.js << EndOfMessage
 /**
  * @license
- * Copyright (c) 2022 Google LLC All rights reserved.
- * Use of this source code is governed by a BSD-style
- * license that can be found in the LICENSE file.
+ * Copyright 2023 NeonFlan LLC
+ * SPDX-License-Identifier: BSD-3-Clause
  */
 ({
 update({}, state, tools) {
