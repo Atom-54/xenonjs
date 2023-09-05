21e05ab519babafdd091018c17fdff787a22f5b2
Scott J. Miles <scott.miles@gmail.com>
2023-09-05 15:54:48 -0700

    add (TOAST) Calendar support

---
+++ pkg/Library/Calendar/Nodes/CalendarNode.js
@@ -0,0 +1,12 @@
+/**
+ * @license
+ * Copyright 2023 NeonFlan LLC
+ * SPDX-License-Identifier: BSD-3-Clause
+ */
+
+export const CalendarNode = {
+  chart: {
+    type: '$library/Calendar/Atoms/Calendar',
+    inputs: []
+  }
+};
