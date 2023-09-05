154915a926afdd073e12d806d750cc81b4f129ab
Maria Kleiner <mashkaaaa@gmail.com>
2023-09-04 21:18:39 -0700

    accordion atom and node

---
+++ pkg/Library/Layout/Nodes/AccordionNode.js
@@ -0,0 +1,11 @@
+/**
+ * @license
+ * Copyright 2023 NeonFlan LLC
+ * SPDX-License-Identifier: BSD-3-Clause
+ */
+export const AccordionNode = {
+  panels: {
+    type: '$library/Layout/Atoms/Accordion',
+    inputs: ['sections']
+  }
+};
