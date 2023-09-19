b6b26ad0fc47ee325c46e44b3b89e77bb8fc88ef
Scott J. Miles <scott.miles@gmail.com>
2023-09-19 16:27:05 -0700

    minimal pwa (for reference)

---
+++ pkg/Run/pwa-trivial/index.js
@@ -0,0 +1,20 @@
+/**
+ * @license
+ * Copyright 2023 NeonFlan LLC
+ * SPDX-License-Identifier: BSD-3-Clause
+ */
+import './config.js';
+import 'xenonjs/Library/common/configKeys.js';
+import 'xenonjs/Library/CoreXenon/Reactor/Atomic/js/logf.js';
+import {main} from 'xenonjs/Run/web/main.js';
+import {connectXenon} from 'xenonjs/Run/web/connectXenon.js';
+import * as Composer from 'xenonjs/Library/CoreXenon/Framework/Composer.js';
+
+(async () => {
+  try {
+    const {xenon} = await connectXenon();
+    await main(xenon, Composer);
+  } catch(x) {
+    console.error(x);
+  }
+})();
