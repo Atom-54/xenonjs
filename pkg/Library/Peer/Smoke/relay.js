30f8615af4b78a9f7305856e8232f5412f28cf13
Scott J. Miles <scott.miles@gmail.com>
2023-09-25 11:07:13 -0700

    libp2p plumbing

---
+++ pkg/Library/Peer/Smoke/relay.js
@@ -0,0 +1,29 @@
+import { createLibp2p } from 'libp2p'
+import { webSockets } from '@libp2p/websockets'
+import { noise } from '@chainsafe/libp2p-noise'
+import { mplex } from '@libp2p/mplex'
+import { yamux } from '@chainsafe/libp2p-yamux'
+
+const node = await createLibp2p({
+  transports: [webSockets()],
+  connectionEncryption: [noise()],
+  streamMuxers: [yamux(), mplex()],
+  addresses: {
+    listen: ['/ip4/0.0.0.0/tcp/0/ws']
+    // TODO check "What is next?" section
+    // announce: ['/dns4/auto-relay.libp2p.io/tcp/443/wss/p2p/QmWDn2LY8nannvSWJzruUYoLZ4vV83vfCBwd8DipvdgQc3']
+  },
+  relay: {
+    enabled: true,
+    hop: {
+      enabled: true
+    },
+    advertise: {
+      enabled: true,
+    }
+  }
+})
+
+console.log(`Node started with id ${node.peerId.toString()}`)
+console.log('Listening on:')
+node.getMultiaddrs().forEach((ma) => console.log(ma.toString()))
