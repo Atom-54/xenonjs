caf9012f04c08e49bb90f04a3d6225c51f7d4282
Maria Kleiner <mashkaaaa@gmail.com>
2023-09-27 19:02:21 -0700

    poly / wiki updates

--- pkg/Library/Polymath/Atoms/WikipediaContent.js
+++ pkg/Library/Polymath/Atoms/WikipediaContent.js
@@ -4,16 +4,20 @@ export const atom = (log, resolve) => ({
    * Copyright 2023 NeonFlan LLC
    * SPDX-License-Identifier: BSD-3-Clause
    */
-  async shouldUpdate({query}, state) {
-    return query;
-  },
   async update({query}, state, {service, isDirty}) {
     if (!isDirty('query')) {
       log.warn('query came again!')
       return;
     }
-    const {title, html} = await service('PolymathService', 'QueryWikipedia', {query});
-    return {title, markup: html};
+    if (query) {
+      const {title, html} = await service('PolymathService', 'QueryWikipedia', {query});
+      return {title, markup: html};
+    } else {
+      return {
+        title: undefined,
+        markup: undefined
+      }
+    }
   }
-  });
+});
   
--- pkg/Library/Polymath/Nodes/PolymathLearnerNode.js
+++ pkg/Library/Polymath/Nodes/PolymathLearnerNode.js
@@ -7,6 +7,6 @@ export const PolymathLearnerNode = {
   PolymathLearner: {
     type: '$library/Polymath/Atoms/PolymathLearner',
     inputs: ['library', 'type', 'source', 'content', 'trigger'],
-    outputs: ['ok']
+    outputs: ['learning', 'ok']
   }
 };
