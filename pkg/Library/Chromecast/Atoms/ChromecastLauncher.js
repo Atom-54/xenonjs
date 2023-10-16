c39c73e4debf314312968a4cabb9047d9fdba0ad
Scott J. Miles <scott.miles@gmail.com>
2023-10-16 11:33:42 -0700

    chromecast support

--- pkg/Library/Chromecast/Atoms/ChromecastLauncher.js
+++ pkg/Library/Chromecast/Atoms/ChromecastLauncher.js
@@ -4,12 +4,8 @@ export const atom = log => ({
  * Copyright 2023 NeonFlan LLC
  * SPDX-License-Identifier: BSD-3-Clause
  */
-async initialize(_, state, {service, output}) {
-  this.initComposer(state, {service, output});
-},
-async initComposer(state, {service, output}) {
-  state.composerId = await service('ChromecastService', 'GetComposerId');
-  output({composer: state.composerId});
+update() {
+
 },
 template: html`
 <google-cast-launcher></google-cast-launcher>
--- pkg/Library/Chromecast/ChromecastComposer.js
+++ pkg/Library/Chromecast/ChromecastComposer.js
@@ -3,18 +3,17 @@
  * Copyright 2023 NeonFlan LLC
  * SPDX-License-Identifier: BSD-3-Clause
  */
-import {Composer} from '../CoreXenon/Reactor/Atomic/js/core/Composer.js';
+import {Composer as BaseComposer} from '../CoreXenon/Reactor/Atomic/js/core/Composer.js';
 import {ChromecastService} from './Services/ChromecastService.js';
 
-export const ChromecastComposer = class extends Composer {
+export const Composer = class {
+  static createComposer(onevent, root) {
+    return new ChromecastComposer(root, onevent);
+  }
+};
+
+export const ChromecastComposer = class extends BaseComposer {
   render(packet) {
-    if (packet.container?.endsWith('Layer$Layer#Container')) {
-      packet.container = 'root';
-    }
-    if (ChromecastService.GetCurrentSession()) {
-      ChromecastService.Render(packet);
-    } else {
-      this.pendingPackets.push(packet);
-    }
+    ChromecastService.Render(packet);
   }
 };
